13c222e89b30234e4b5d304294269b98bf5ee3ae
Rohit R <41517713+Gateway2745@users.noreply.github.com>
2021-08-24 12:15:35 -0400

    correct cli overrides

--- run.py
+++ run.py
@@ -7,11 +7,10 @@ def main():
     if len(sys.argv) == 1:
         raise Exception("Please pass path to config file!")
 
-    cfg = OmegaConf.load(sys.argv[1])
-    sys.argv = sys.argv[1:]
-
-    cli_cfg = OmegaConf.from_cli()
-    CFG = OmegaConf.merge(cfg, cli_cfg)
+    CFG = OmegaConf.load(sys.argv[1])
+    cli_cfg = OmegaConf.from_cli(sys.argv[2:])
+    
+    CFG = OmegaConf.merge(CFG, cli_cfg)
     print(OmegaConf.to_yaml(CFG))  
 
     return train(CFG)
