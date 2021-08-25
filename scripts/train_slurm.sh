c511407375235523969024c2fae21d7cec3045a2
Rohit R <41517713+Gateway2745@users.noreply.github.com>
2021-08-25 11:04:20 -0400

    revise slurm script

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --gres=gpu:2
 #SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=01:00:00
+#SBATCH --time=00:20:00
 
 module load python/3.8.10
 module load gcc/8.4.0
@@ -12,8 +12,9 @@ module load cuda/10.2
 module load cudacore/.11.1.1
 module load cudnn/8.2.0
 source /home/rohitram/pytorch_nightly/bin/activate
-mkdir $SLURM_TMPDIR/folds_cache
-cd $SLURM_TMPDIR
-unzip -qq /home/rohitram/projects/def-mudl/rohitram/sbu_dataset.zip -d $SLURM_TMPDIR/sbu_dataset
-CUDA_LAUNCH_BLOCKING=1 srun python /home/rohitram/scratch/Key-Actor-Detection/run.py /home/rohitram/scratch/Key-Actor-Detection/configs/sbu.yaml "dataset_path=$SLURM_TMPDIR/sbu_dataset" "training.save_dir=$SLURM_TMPDIR/mlruns" "caching.folds_cache_path=$SLURM_TMPDIR/folds_cache"
+
+srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 unzip -qq /home/rohitram/projects/def-mudl/rohitram/sbu_dataset.zip -d $SLURM_TMPDIR/sbu_dataset
+
+CUDA_LAUNCH_BLOCKING=1 srun python /home/rohitram/scratch/Key-Actor-Detection/run.py /home/rohitram/scratch/Key-Actor-Detection/configs/sbu.yaml "dataset_path=$SLURM_TMPDIR/sbu_dataset" "training.save_dir=$SLURM_TMPDIR/mlruns" 
+
 cp -r $SLURM_TMPDIR/mlruns /home/rohitram/scratch
