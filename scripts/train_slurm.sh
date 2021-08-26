bf4b36676496bccd3030d8ceb959a0a021673c5b
Rohit R <41517713+Gateway2745@users.noreply.github.com>
2021-08-26 03:47:51 -0400

    revise slurm script:CUDA_LAUNCH_BLOCKING=1 errors in ddp

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -11,10 +11,14 @@ module load gcc/8.4.0
 module load cuda/10.2
 module load cudacore/.11.1.1
 module load cudnn/8.2.0
-source /home/rohitram/pytorch_nightly/bin/activate
+source /home/rohitram/pytorch/bin/activate
 
 srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 unzip -qq /home/rohitram/projects/def-mudl/rohitram/sbu_dataset.zip -d $SLURM_TMPDIR/sbu_dataset
 
-CUDA_LAUNCH_BLOCKING=1 srun python /home/rohitram/scratch/Key-Actor-Detection/run.py /home/rohitram/scratch/Key-Actor-Detection/configs/sbu.yaml "dataset_path=$SLURM_TMPDIR/sbu_dataset" "training.save_dir=$SLURM_TMPDIR/mlruns" 
+srun python /home/rohitram/scratch/Key-Actor-Detection/run.py /home/rohitram/scratch/Key-Actor-Detection/configs/sbu.yaml "dataset_path=$SLURM_TMPDIR/sbu_dataset" "training.save_dir=$SLURM_TMPDIR/mlruns" 
 
 cp -r $SLURM_TMPDIR/mlruns /home/rohitram/scratch
+
+
+
+
